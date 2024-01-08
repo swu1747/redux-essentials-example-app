d9e20b933229497ef67e30513bd38360e7b142db
swu1747 <swu1747@gmail.com>
2024-01-07 21:59:56 -0500

    in progress

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom'
 
 export const Navbar = () => {
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
-
+        <div className="navLinks">
+          <Link to='/'>Posts</Link>
+        </div>
         <div className="navContent">
           <div className="navLinks"></div>
         </div>
--- src/app/SinglePostPage.js
+++ src/app/SinglePostPage.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { postList } from './postslice'
-const SinglePage = () => {
+const SinglePage = (props) => {
+    const postlist = useSelector(postList)
+    const item = props.match.params.postId
+
     const PageInfo = () => {
+        const post = postlist.find((post) => post.id === item)
 
+        if (!post) {
+            return (
+                <section>
+                    <h2>Post not found!</h2>
+                </section>
+            )
+        }
+        return (<section>
+            <article className="post">
+                <h2>{post.title}</h2>
+                <p className="post-content">{post.content}</p>
+            </article>
+        </section>)
     }
-
+    return (<>{PageInfo()}</>)
 }
 
 export default SinglePage
